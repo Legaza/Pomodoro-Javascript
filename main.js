fd7197018b9f40098f081fbae3b4ee2b493639a0
Leonardo Garcia <LegazaGZ@outlook.com>
2022-02-27 18:41:10 -0600

    Se agrega un contador de 25 min que vaya disminuyendo, funciona deacuerdo a si la tarea esta asignada o no y se renderiza el tiempo en formato

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let current = null;
 const bAdd = document.querySelector('#bAdd');
 const itTask = document.querySelector('#itTask');
 const form = document.querySelector('#form');
+const taskName = document.querySelector('#time #taskName');
 
 form.addEventListener('submit', e=>{
     e.preventDefault();//cancela el evento por default
@@ -43,4 +44,47 @@ function renderTasks(){
 
     const tasksContainer = document.querySelector("#tasks");
     tasksContainer.innerHTML = html.join("");
+
+    const startButtons = document.querySelectorAll('.task .start-button');
+    startButtons.forEach(button =>{
+        button.addEventListener('click', e =>{
+            if(!timer){
+                const id = button.getAttribute('data-id');
+                startButtonsHandler(id);
+                button.textContent = 'In Progress...';
+            }
+        });
+    });
+}
+
+function startButtonsHandler(id){
+    time = 25 * 60;
+    current = id;
+    const taskIndex = tasks.findIndex( task => task.id === id);
+    
+    taskName.textContent = tasks[taskIndex].title;
+
+    timer = setInterval(()=>{
+        timeHandler(id);
+    },1000);
+}
+
+function timeHandler(id){
+    time--;
+    renderTime();
+
+    if(time === 0){
+        clearInterval(timer);
+        current = null;
+        taskName.textContent="";
+        renderTime();
+    }
+}
+
+function renderTime(){
+    const timeDiv = document.querySelector('#time #value');
+    const minutes = parseInt(time/60);
+    const seconds = parseInt(time%60);
+
+    timeDiv.textContent=`${minutes< 10 ? '0' : ''}${minutes}:${seconds <10 ? '0': ''}${seconds}`;
 }
